bfed92acb6fc230f39cbce751d41e9be1cf2e73f
yashsaini7060 <yk18052000@gmail.com>
2024-05-09 18:43:31 +0530

    links changed

--- src/helper/api.js
+++ src/helper/api.js
@@ -21,12 +21,12 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`
   },
   airPollution(lat, lon){
-    return `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`
+    return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`
   },
   reverseGeo(lat, lon){
-    return `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`
+    return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`
   },
   geo(query){
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10`
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10`
   }
 }
